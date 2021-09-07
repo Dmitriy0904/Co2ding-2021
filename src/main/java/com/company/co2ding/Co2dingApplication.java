fcdf0880a0f80f335b8f2e68aa1c44b250b37367
Dmitriy <dmitriygridin0904@gmail.com>
2021-09-07 11:19:22 +0300

    Initital spring project commit

---
+++ src/main/java/com/company/co2ding/Co2dingApplication.java
@@ -0,0 +1,13 @@
+package com.company.co2ding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2dingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2dingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/co2ding/Co2dingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.co2ding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2dingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
